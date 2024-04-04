dc053f47b2b71d490be95c4934f5d9bcdd9d9e10
Tej <46010260+TejGandham@users.noreply.github.com>
2024-04-04 16:45:00 -0700

    Create note display and interaction components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import NotesContainer from './features/notes/components/NotesContainer';
+import theme from './styles/theme';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <NotesContainer />
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ src/features/notes/components/NotesContainer.tsx
@@ -0,0 +1,64 @@
+import { Box, Flex } from '@chakra-ui/react';
+import { useNotesState } from '../../../hooks/useNotesState';
+import { useSidebarState } from '../../../hooks/useSidebarState';
+import Header from '../../../components/layout/Header';
+import Sidebar from '../../../components/layout/Sidebar';
+import NoteContent from '../../../components/notes/NoteContent';
+import { COLORS } from '../../../utils/constants';
+import '../../../components/layout/styles/layout.css';
+
+const NotesContainer = () => {
+  const {
+    notes,
+    currentNoteId,
+    currentNote,
+    isEditing,
+    editedContent,
+    editedTitle,
+    setEditedContent,
+    setEditedTitle,
+    selectNote,
+    createNewNote,
+    deleteNote,
+    startEditing,
+    saveNote,
+  } = useNotesState();
+
+  const { isSidebarOpen, toggleSidebar } = useSidebarState();
+
+  return (
+    <Box minH="100vh" bg={COLORS.bgColor} className="bg-texture">
+      <Flex h="100vh" flexDirection="column">
+        <Header toggleSidebar={toggleSidebar} />
+
+        <Flex flex="1" overflow="hidden">
+          {isSidebarOpen && (
+            <Sidebar
+              notes={notes}
+              currentNoteId={currentNoteId}
+              onSelectNote={selectNote}
+              onCreateNote={createNewNote}
+              onDeleteNote={deleteNote}
+            />
+          )}
+
+          <Box flex="1" p={6} overflowY="auto" className="note-transition">
+            <NoteContent
+              currentNote={currentNote}
+              isEditing={isEditing}
+              editedTitle={editedTitle}
+              editedContent={editedContent}
+              setEditedTitle={setEditedTitle}
+              setEditedContent={setEditedContent}
+              startEditing={startEditing}
+              saveNote={saveNote}
+              createNewNote={createNewNote}
+            />
+          </Box>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+export default NotesContainer;
