e3d736ce91c3a993b72ddd711e65cc5ab7ef6db8
Tej <46010260+TejGandham@users.noreply.github.com>
2025-01-11 10:00:00 -0500

    chore: Remove test file used for pre-commit hook testing

--- ui/src/test-formatting.tsx
+++
@@ -1,9 +0,0 @@
-// File with bad formatting
-import React from "react"
-
-const TestComponent = ( ) => {
-  const   message    =     "Testing lint-staged"
-  return <div>{ message }</div>
-}
-
-export default TestComponent
