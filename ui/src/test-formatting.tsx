df654f65958bcfeea7b0bb8cecc5f8d44d149d98
Tej <46010260+TejGandham@users.noreply.github.com>
2025-08-18 17:03:26 -0400

    feat: Add Husky pre-commit hooks with lint-staged
    - Configure Husky v9 for pre-commit hooks
    - Set up lint-staged to run ESLint and Prettier on staged files
    - Automatically fix formatting and linting issues before commit
    - Prevent commits with linting errors

---
+++ ui/src/test-formatting.tsx
@@ -0,0 +1,9 @@
+// File with bad formatting
+import React from "react"
+
+const TestComponent = ( ) => {
+  const   message    =     "Testing lint-staged"
+  return <div>{ message }</div>
+}
+
+export default TestComponent
